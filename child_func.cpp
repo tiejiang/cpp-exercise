dd395177411313099e80937a72a1bbe1ab8007e8
tiejiang <315904145@qq.com>
2017-12-25 07:36:59 +0800

    point and function excerise

---
+++ child_func.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: child_func.cpp
+	> Author: tiejiang
+    > Mail: 315904145@qq.com 
+	> Created Time: 2017年12月24日 星期日 09时52分11秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+void printFuncTest(void){
+    
+    cout<< "this func is an test func, nothing is now"<< endl;
+}
--- cpp_common_excerise.cpp
+++ cpp_common_excerise.cpp
@@ -2,13 +2,60 @@
 
 using namespace std;
 
-#define LENGTH 100
-#define HEIGHT 200
+//#define LENGTH 100
+//#define HEIGHT 200
 
+void constTest(){
+    
+    const int LENGTH = 6; 
+    const int HEIGHT = 6;
+    cout<< "area= "<< LENGTH*HEIGHT<< endl;
+}
+
+//extern void printFuncTest();
+
+void loopTest(){
+    
+    for(int i=0; i<9; i++){
+        cout<< "i= "<< i<<endl;
+        if(i==2){
+            cout<< "now i = 2, jump to i = 4 judge"<< endl;
+            i = 4;
+            continue;
+        }
+        if(i==8){
+            cout<< "now i = 8, break"<< endl;
+        }
+    }
+}
+
+void judgeTest(){
+
+    int flag = 4;
+    flag<5 ? cout<<"right"<< endl : cout<<"wrong" << endl; 
+}
+
+void swap(int *x, int *y){
+
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+
+    return;
+}
 
 int main(){
     
-    cout<< "area= "<< LENGTH*HEIGHT<< endl;
-    
+    //cout<< "area= "<< LENGTH*HEIGHT<< endl;
+    //constTest();
+   // printFuncTest();
+   // loopTest();
+    //judgeTest();
+    int a = 10;
+    int b = 12;
+    swap(&a, &b);
+    cout<< "a= "<< a<< " b= "<<b <<endl;
+
     return 0;
 }
